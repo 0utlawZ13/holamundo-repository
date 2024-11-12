031c7b3b1eb846b01f19604167fe24c433c97720
Cristian Alemán <bladimiralemanc@gmail.com>
2024-11-12 15:21:52 -0500

    feat: :sparkles: Programmed basic classes and function to deploy an "hola mundo" with a direction

--- holamundo/src/main/java/com/example/holamundo/HolamundoApplication.java
+++ holamundo/src/main/java/com/example/holamundo/HolamundoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HolamundoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HolamundoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HolamundoApplication.class, args);
+    }
 }
